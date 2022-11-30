fa4a9d3925e608e8ce1b23cc73c6e9e4aea2f1dd
Kaito_OwO <advestankv@gmail.com>
2022-11-30 22:19:10 +0100

    True initial commit

---
+++ Geometry Project/Geometry Project.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Game.h"
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "ChronoSpacer");
+    // Initialise everything below
+
+    // Game loop
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Process any input event here
+
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    //...
+                    break;
+                default:
+                    break;
+            }
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear();
+        // Whatever I want to draw goes here
+        window.display();
+    }
+
+}
