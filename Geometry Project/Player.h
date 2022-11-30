9d3bf00f29d5f8f0d3f3e61846087c2d62a0d78e
Kaito_OwO <advestankv@gmail.com>
2022-11-30 22:43:10 +0100

    Revert ".h update"
    This reverts commit c82f2b555a9663364052ecebd3b0ef150471dbb8.

--- Geometry Project/Player.h
+++ Geometry Project/Player.h
@@ -3,13 +3,9 @@
 class Player
 {
 public:
-	sf::Vector2f position;
-
-	Player(sf::Vector2f& initialPosition, float& size, sf::Color color = sf::Color::Red);
+	Player();
 	~Player();
 
 private:
-	float size;
-	sf::Color color;
 
 };
