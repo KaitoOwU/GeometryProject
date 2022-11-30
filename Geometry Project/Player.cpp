0aa2b81a1cd344eea8d3cc89bc1949da41ad2821
Kaito_OwO <advestankv@gmail.com>
2022-11-30 22:40:45 +0100

    Player Constructor created

--- Geometry Project/Player.cpp
+++ Geometry Project/Player.cpp
@@ -1,7 +1,16 @@
+#include <SFML/Graphics.hpp>
 #include "Player.h"
 
-Player::Player()
+Player::Player(sf::Vector2f& initialPosition, float& size, sf::Color color = sf::Color::Red)
 {
+	this->position = initialPosition;
+	this->size = size;
+	this->color = color;
+
+	sf::CircleShape shape;
+	shape.setRadius(size);
+	shape.setPosition(initialPosition);
+	shape.setFillColor(color);
 }
 
 Player::~Player()
